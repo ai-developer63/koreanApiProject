bc5e84f77cea3b23b37327edde406deb25085eec
Asus <bsubash723@gmail.com>
2024-02-02 14:27:37 +0545

    started commiting

---
+++ src/main/java/com/example/demo/ActualKoreanappApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActualKoreanappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActualKoreanappApplication.class, args);
+	}
+
+}
